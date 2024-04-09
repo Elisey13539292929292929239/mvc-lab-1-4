8395112c0b800991b15ae33320fbc1368738c738
Елисей Угримов <ugrimov35335@gmail.com>
2024-04-09 10:37:27 +0200

    Удалены все файлы из репозитория

--- zad2/controllers/error.js
+++
@@ -1,3 +0,0 @@
-exports.getNotFoundPage = (req, res) => {
-    res.status(404).render('NotFound', { title: 'Not found' });
-  };
--- zad2/controllers/students.js
+++
@@ -1,8 +0,0 @@
-exports.getAddNewStudentPage = (req, res) => {
-    res.render('Home', { title: 'Add new student' });
-  };
-  
-  exports.addStudent = (req, res) => {
-    const { fullName, code, fieldOfStudies } = req.body;
-    res.redirect('/students-list');
-  };
